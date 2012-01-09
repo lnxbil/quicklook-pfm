ed49e22c3dd0befff42fad392f79bd7ddce55c9c
Andreas Steinel <A.Steinel@gmail.com>
2012-01-09 23:42:27 +0100

    Added license to all files

--- src/image.h
+++ src/image.h
@@ -1,13 +1,25 @@
-//
-//  image.h
-//  Quicklook-PFM
-//
-//  Created by Andreas Steinel on 17.01.09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/*
+ *  image.h
+ *  Quicklook-PFM
+ * 
+ * Copyright (C) 2008-2012 Andreas Steinel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 
 #import <Cocoa/Cocoa.h>
-
 
 
 // This class implements the Picture Map Reader (PMReader)
